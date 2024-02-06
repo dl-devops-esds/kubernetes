610e67cb5e769f23e33ad99e71f1729058cf4591
dl-devops-esds <127200003+dl-devops-esds@users.noreply.github.com>
2024-02-06 15:49:52 +0530

    System upgrade command removed ubuntu-k8s-master.sh.

--- ubuntu-k8s-master.sh
+++ ubuntu-k8s-master.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Update the system
-sudo apt-get update -y
-sudo apt-get upgrade -y
-
 # Disable swap
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
@@ -12,7 +8,6 @@ sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo apt-get install -y docker.io
 
 # Add Kubernetes repository and install Kubernetes components
-sudo apt-get update -y
 sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
