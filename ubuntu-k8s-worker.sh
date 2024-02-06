b36c3cdfb9bb11767a023da2989180f4b1664e0a
dl-devops-esds <127200003+dl-devops-esds@users.noreply.github.com>
2024-02-06 15:49:07 +0530

    Remove Version's from ubuntu-k8s-worker.sh

--- ubuntu-k8s-worker.sh
+++ ubuntu-k8s-worker.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Update the system
-sudo apt-get update -y
-sudo apt-get upgrade -y
-
 # Disable swap
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
@@ -12,9 +8,8 @@ sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo apt-get install -y docker.io
 
 # Add Kubernetes repository and install Kubernetes components
-sudo apt-get update -y
 sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
-sudo apt-get install -y kubelet=1.28.2 kubeadm=1.28.2 kubectl=1.28.2
+sudo apt-get install -y kubelet kubeadm kubectl
